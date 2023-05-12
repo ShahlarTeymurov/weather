3a50ff46f86f2114577e567bb526f1ebc632dceb
ShahlarTeymurov <tshahlar@gmail.com>
2023-05-12 19:19:54 +0000

    app

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
 "use strict";
 $(document).ready(function () {
 
+    
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=a48c3222708fb7c8af2bfc6f5e4302bb&units=metric`
+            async function myweather() {
+                let responce = await fetch(url)
+                let data = await responce.json()
+                $("#city").html(data.name)
+                $("#temp").html(data.main.temp)
+                $("#feels").html(data.main.feels_like)
+                $("#myWeather").val("")
+                console.log(data)
+            }
+            myweather()
+        }
+    })
+
+    $("#btn").click(function (e) {
+        e.preventDefault()
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=a48c3222708fb7c8af2bfc6f5e4302bb&units=metric`
+        async function myweather() {
+            let responce = await fetch(url)
+            let data = await responce.json()
+            $("#city").html(data.name)
+            $("#temp").html(data.main.temp)
+            $("#feels").html(data.main.feels_like)
+            $("#myWeather").val("")
+            console.log(data)
+        }
+        myweather()
+
+    })
+
 });
